9370ced94770a5b05d62357e09bff22c8ca41a3f
unknown <jahnav3@gmail.com>
2023-05-28 02:59:16 +0530

    copy icon 📋

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = function(){
     const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
     const btn = document.getElementById("btn");
+    const copybtn = document.getElementById("copy");
     const colorTxt = document.querySelector(".color");
     btn.addEventListener("click", () => {
         var color = "#";
@@ -9,7 +10,10 @@ window.onload = function(){
         }
         colorTxt.textContent = color;
         document.body.style.backgroundColor = color;
+    })
 
+    copybtn.addEventListener('click', () => {
+        navigator.clipboard.writeText(colorTxt.textContent)
     })
 
 }
